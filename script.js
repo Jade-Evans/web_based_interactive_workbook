f472e83c335424a47888c272018ba01f8291dffc
Jade-Evans <jpgabb@msn.com>
2026-01-15 20:42:12 +0000

    fix: correct text for sustainability notes headers on multiple pages

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const headerLogos = document.querySelectorAll(".logo");
-headerLogos.forEach(logo => logo.textContent = "🌱 Skillroots");
+headerLogos.forEach(
+    logo => {
+    logo.textContent = "🌱 SKILLROOTS";
+    logo.style.fontSize = "2rem";
+    logo.style.color = "white";
+});
+
+const sustainabilityNotesHeaders = document.querySelectorAll(".sustainabilityNotesHeader");
+sustainabilityNotesHeaders.forEach(header=>{
+    header.textContent = "Notes on Sustainability";
+})
