277bd8ddca4e32ddda833c6e41e54510f7277cc0
Jade-Evans <jpgabb@msn.com>
2026-01-17 19:40:32 +0000

    init: use doc to store the guidance written originally in fixed modules_template for later retrieval

---
+++ study_guidance.js
@@ -0,0 +1,24 @@
+export const guidance = {
+  readAndSummarise: `
+    If you had to summarise this module using only up to 5 facts,
+    what would they be? Keep them simple, light, in your own words.
+    You don't have to do all five; we recommend not doing any more
+    as this shouldn't turn into a laborious task.
+  `,
+  practiceAndReinforce: `
+    Engaging in practice exercises is a very effective way to reinforce
+    what you've learned. It's the difference between actively growing
+    connections and simply reading words on a page.
+  `,
+  recordAndRemember: `
+    Sometimes in learning a topic, we come across an idea or information
+    that we want to revert back to later. Use the sticky notes below as
+    cards to record your learning.
+  `,
+  sustainability: `
+    Add sustainability notes here.
+  `,
+  accessibility: `
+    Add accessibility notes here.
+  `
+};
